1dcb59bf651988eaf5ffc831e61308d0ff8c4f49
Bruno Guilherme dos Santos <belmondo_dev@outlook.com>
2024-01-11 23:06:43 -0300

    fix chars on about description

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,15 +4,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <article>
-        <p>
-          I'm a Software Engineer with over 10 years of experience, having collaborated on diverse projects spanning digital solutions,
-          software development, and IT services. My recent focus has been on JS, React, and React Native, although I'm proficient in PHP,
-          MySQL, and various other technologies. Fueled by curiosity, I'm dedicated to discovering creative solutions for project success.
-        </p>
-        <p>
-          I enjoy positive collaboration and exchanging experiences with teammates. For me, interactivity is not just work but also fun. In
-          my free time, I channel my creativity into game development.
-        </p>
+        I'm a Software Engineer with over 10 years of experience, having collaborated on diverse projects spanning digital solutions,
+        software development, and IT services. My recent focus has been on JS, React, and React Native, although I'm proficient in PHP,
+        MySQL, and various other technologies. Fueled by curiosity, I'm dedicated to discovering creative solutions for project success. I
+        enjoy positive collaboration and exchanging experiences with teammates. For me, interactivity is not just work but also fun. In my
+        free time, I channel my creativity into game development.
       </article>
     </main>
   )
